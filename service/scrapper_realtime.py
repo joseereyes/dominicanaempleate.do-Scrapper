3eac5677daa73c3e3157ffc1ba3263237b317580
joseereyes <ryusatoshi@yahoo.com>
2021-10-12 22:40:30 -0400

    solved time conflict

--- service/scrapper_realtime.py
+++ service/scrapper_realtime.py
@@ -22,93 +22,94 @@ default_app = firebase_admin.initialize_app(
 
 def realtime_scrapper():
     
-    from datetime import date
-    todayDate = date.today()
+    from datetime import datetime
+    todayDate = datetime.now()
+ 
     
-    driver = webdriver.Chrome(PATH)
+    # driver = webdriver.Chrome(PATH)
 
-    try:
-        driver.get(url)
-        time.sleep(2)
-        jobs_list = driver.find_elements_by_class_name("articulo")
-    except:
-        driver.close()
-        time.sleep(exception_time)
-        realtime_scrapper()
+    # try:
+    #     driver.get(url)
+    #     time.sleep(2)
+    #     jobs_list = driver.find_elements_by_class_name("articulo")
+    # except:
+    #     driver.close()
+    #     time.sleep(exception_time)
+    #     realtime_scrapper()
 
     
-    jobs_arr = []
+    # jobs_arr = []
 
-    for x in jobs_list:
+    # for x in jobs_list:
         
      
 
-        title = x.find_element_by_class_name("resumido").text
-        category = x.find_element_by_class_name("categoria").text
-        date = x.find_element_by_class_name("date").text
-        desc = x.find_element_by_class_name("resumen").text
-        loc = x.find_element_by_class_name("provincia").text
-        href = x.find_element_by_class_name("ver-mas").get_attribute('href')
+    #     title = x.find_element_by_class_name("resumido").text
+    #     category = x.find_element_by_class_name("categoria").text
+    #     date = x.find_element_by_class_name("date").text
+    #     desc = x.find_element_by_class_name("resumen").text
+    #     loc = x.find_element_by_class_name("provincia").text
+    #     href = x.find_element_by_class_name("ver-mas").get_attribute('href')
 
-        object = {
-            "title": title.replace("/"," "),
-            "date": date,
-            "desc": desc,
-            "loc": loc,
-            "href": href,
-            "category": category,
-            "dateReg" : str(todayDate)
+    #     object = {
+    #         "title": title.replace("/"," "),
+    #         "date": date,
+    #         "desc": desc,
+    #         "loc": loc,
+    #         "href": href,
+    #         "category": category,
+    #         "dateReg" : str(todayDate)
        
-        }
-        jobs_arr.append(object)
+    #     }
+    #     jobs_arr.append(object)
         
 
 
-    time.sleep(1)
-    driver.delete_all_cookies()
-    driver.close()
-    driver.quit()
+    # time.sleep(1)
+    # driver.delete_all_cookies()
+    # driver.close()
+    # driver.quit()
 
-    for i, item in enumerate(jobs_arr):
+    # for i, item in enumerate(jobs_arr):
 
-        try:
-            index = webdriver.Chrome(PATH)
-            index.get(item["href"])
-            time.sleep(2)
-            job_body = index.find_element_by_class_name("contenido")
-            item["content"] = job_body.text
+    #     try:
+    #         index = webdriver.Chrome(PATH)
+    #         index.get(item["href"])
+    #         time.sleep(2)
+    #         job_body = index.find_element_by_class_name("contenido")
+    #         item["content"] = job_body.text
        
-            email = re.findall(r"[a-zA-Z0-9\.\-+_]+@[a-zA-Z0-9\.\-+_]+\.[a-zA-Z]+",item["content"])
+    #         email = re.findall(r"[a-zA-Z0-9\.\-+_]+@[a-zA-Z0-9\.\-+_]+\.[a-zA-Z]+",item["content"])
 
-            if len(email) > 0:
+    #         if len(email) > 0:
 
-                item["email"] = email[0]
-                ref = db.reference('Jobs')
+    #             item["email"] = email[0]
+    #             ref = db.reference('Jobs')
                 
-                data = ref.order_by_child("title").equal_to(item["title"]).get()
+    #             data = ref.order_by_child("title").equal_to(item["title"]).get()
                 
-                if data:
-                    for key,values in data.items():
-                        if item["date"] != values["date"]:
-                            ref.push(item)
-                else:
-                    ref.push(item)
+    #             if data:
+    #                 for key,values in data.items():
+    #                     if item["date"] != values["date"]:
+    #                         ref.push(item)
+    #             else:
+    #                 ref.push(item)
                     
-            else:
-                del jobs_arr[i]
+    #         else:
+    #             del jobs_arr[i]
             
-            index.delete_all_cookies()
-            index.close()
-            index.quit()
+    #         index.delete_all_cookies()
+    #         index.close()
+    #         index.quit()
             
-            del index
-            gc.collect()
+    #         del index
+    #         gc.collect()
             
-        except:
-            index.close()
-            time.sleep(exception_time)
-            realtime_scrapper()
+    #     except:
+    #         index.close()
+    #         time.sleep(exception_time)
+    #         realtime_scrapper()
             
     
-    time.sleep(300)
-    realtime_scrapper()
+    # time.sleep(300)
+    # realtime_scrapper()
